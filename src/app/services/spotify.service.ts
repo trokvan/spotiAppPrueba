bdeae5ec9ac0142ace75d0744665e5abb6f415c7
Ivan Morales <greativann@gmail.com>
2020-06-08 19:45:57 -0500

    agregar función getQuery en el servicio para no repetir lo mismo al hacer las peticiones

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -7,22 +7,31 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class SpotifyService {
-  //Constante que contiene los headers necesarios para que spotify pueda regresarnos la informacion
-  private _HEADERS=new HttpHeaders({
-    'Authorization': 'Bearer BQC16LEcC2yidb-BaD0uKtLLEqyG5W3QuKEO4A7Lo9Aek-_-VOS2oVe1LbP9Le1hw9ZW72zleDkJhTZ7Bss'
-  });
+  
   constructor(private _http:HttpClient) { 
     //console.log("Servicio listo");
   }
 
-  getNuevosLanzamientos(){
+  /*Funcion para obtener la consulta que se va a realizar a la API,
+   de esta forma no repetimos codigo cada vez que necesitemos realizar una petición*/
+  getQuery(query:string){
+    //Constante que contiene los headers necesarios para que spotify pueda regresarnos la informacion
+    const HEADERS=new HttpHeaders({
+      'Authorization': 'Bearer BQC16LEcC2yidb-BaD0uKtLLEqyG5W3QuKEO4A7Lo9Aek-_-VOS2oVe1LbP9Le1hw9ZW72zleDkJhTZ7Bss'
+    });
+    const url=`https://api.spotify.com/v1/${ query }`;
+
     //En la función get después de la url podemos especificar las opciones, en este caso los headers, retornamos todo el observable
-    return this._http.get('https://api.spotify.com/v1/browse/new-releases?limit=30', { headers:this._HEADERS })
+    return this._http.get(url,{headers:HEADERS});
+  }
+
+  getNuevosLanzamientos(){ 
+    return this.getQuery('browse/new-releases?limit=30')
           .pipe(map(resultados=>resultados['albums'].items)); //map nos ayudará a obtener sólo lo que necesitamos
   }
 
   getAristas(termino:string){
-    return this._http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,{headers:this._HEADERS})
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
           .pipe(map(resultados=>resultados['artists'].items));
   }
 }
